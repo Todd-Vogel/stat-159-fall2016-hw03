a324fa16d0e4b4926417989abcf5a06a504123fd
Todd Vogel <tjvogel@berkeley.edu>
2016-10-14 16:33:44 -0700

    adding tolerance to tests

--- code/tests/test-regression.R
+++ code/tests/test-regression.R
@@ -1,7 +1,9 @@
+source("../functions/regression-functions.R")
+
 context("Test for Residual Sum of Squares")
 test_that("Residual Sum of Squares works", {
   reg=lm(mpg~hp+disp, data = mtcars)
-   expect_equal(residual_sum_squares(reg), 283.4934)	
+   expect_equal(residual_sum_squares(reg), 283.4934, tolerance = .05)	
 })
 
 summary(lm(mpg~hp+disp, data = mtcars))
@@ -9,23 +11,23 @@ summary(lm(mpg~hp+disp, data = mtcars))
 context("Test for Total Sum of Squares")
 test_that("Total Sum of Squares works", {
   reg=lm(mpg~hp+disp, data = mtcars)
-   expect_equal(total_sum_squares(reg), 1126.047)	
+   expect_equal(total_sum_squares(reg), 1126.047, tolerance = .05)	
 })
 
 context("Test for R2")
 test_that("R2 works", {
   reg=lm(mpg~hp+disp, data = mtcars)
-   expect_equal(r_squared(reg), 0.7482402)	
+   expect_equal(r_squared(reg), 0.7482402, tolerance = .05)	
 })
 
 context("Test for F Statistic")
 test_that("F Statistic works", {
   reg=lm(mpg~hp+disp, data = mtcars)
-   expect_equal(f_statistic(reg), 43.09458)	
+   expect_equal(f_statistic(reg), 43.09458, tolerance = .05)	
 })
 
 context("Test for Residual Standard Error")
 test_that("Residual Standard Error works", {
   reg=lm(mpg~hp+disp, data = mtcars)
-   expect_equal(residual_std_error(reg), 3.126601)	
+   expect_equal(residual_std_error(reg), 3.126601, tolerance = .05)	
 })
